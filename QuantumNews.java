2c61a35492e600a92d0d60c58e4dd38ff2b1e88f
bucky300 <80122295+bucky300@users.noreply.github.com>
2021-03-07 16:20:04 -0500

    Add files via upload

---
+++ QuantumNews.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physicsNews;
+
+/**
+ *
+ * @author kent
+ */
+public class QuantumNews {
+ public static void main(String[]args){
+     System.out.println("Quantum tunneling)");
+     System.out.println("Overcoming the Coulomb Barrier");   
+ }  
+}
